55fbf6763cfa7de1f78126fa4c03e2d777432d25
Akshay-Doultani <161005208+Akshay-Doultani@users.noreply.github.com>
2024-06-15 16:40:16 +0530

    Update SiginForm.tsx

--- src/auth/forms/SiginForm.tsx
+++ src/auth/forms/SiginForm.tsx
@@ -147,7 +147,7 @@ const SigninForm = () => {
               to="/sign-up"
               
               className="text-primary-500 text-small-semibold ml-1">
-              Sign up
+              Sign-up 
             </Link>
           </p>
         </form>
